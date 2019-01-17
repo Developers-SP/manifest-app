f75103928b61bcb7fea76af625b07e3dd3aab0b0
Douglas Santos dos Reis <douglas.santosreis@gmail.com>
2019-01-17 00:40:44 -0200

    create the routes to pilots and planes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
-const userRoute = require('./user.route');
+const userRoute = require('./user.route')
+, pilotRoute = require('./pilot.route')
+, planeRoute = require('./plane.route');
 
 module.exports = (app) => {
-  app.use('/', userRoute);
+  app.use('/user', userRoute);
+  app.use('/pilot', pilotRoute);
+  app.use('/plane', planeRoute);
 };
