4a0ecbe137c8ea758a4fe8cbe788d4c7e1bba823
Douglas Santos dos Reis <douglas.santosreis@gmail.com>
2019-01-17 00:48:01 -0200

    Remove encrypt in middlewares

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,5 @@
 const logger = require('./logger');
-const encrypt = require('./encrypt');
 
 module.exports = app => {
   app.use((req, res, next) => logger(req, res, next));
-  app.use((req, res, next) => encrypt(req, res, next));
 };
